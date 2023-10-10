7182b3b5336805bba3d71a85b9132f2427a1e21c
jkhaimi <jesse.haimi@icloud.com>
2023-10-10 18:00:19 +0200

    4.4 done

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,7 +1,13 @@
+
 const dummy = (blogs) => {
     return 1
   }
   
+const totalLikes = (blogs) => {
+    const summedLikes = blogs.reduce((total, blog) => total + blog.likes, 0);
+    return blogs.length === 0 ? 0 : summedLikes;
+  }
+
   module.exports = {
-    dummy
+    dummy, totalLikes
   }
