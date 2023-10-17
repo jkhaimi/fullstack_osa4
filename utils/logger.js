af95bd108d66d73a865aa6f52461296e33eb66ac
jkhaimi <jesse.haimi@icloud.com>
2023-10-17 13:31:21 +0200

    4.19 done

--- utils/logger.js
+++ utils/logger.js
@@ -5,11 +5,18 @@ const info = (...params) => {
   }
 }
 
-const error = (...params) => {
+const error = (error, request, response, next) => {
+  logger.error(error.message)
 
-  if (process.env.NODE_ENV !== 'test') { 
-    console.error(...params)
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
+  } else if (error.name ===  'JsonWebTokenError') {
+    return response.status(400).json({ error: 'token missing or invalid' })
   }
+
+  next(error)
 }
 
 module.exports = {
